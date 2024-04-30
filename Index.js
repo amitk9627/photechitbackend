555cedf4f20cfdd8d99de527fee815e23d7d615a
amitk9627 <ramitgzp56@gmail.com>
2024-05-01 03:54:35 +0530

    custom

--- Index.js
+++ Index.js
@@ -1,9 +1,9 @@
 const app = require("./app.js");
 require('dotenv').config();
 const mongoose = require("mongoose");
-const port = process.env.port ?? 3000;
+const port =  3000;
 const mongoDB = async () => {
-  await mongoose.connect( process.env.mongoDBurl);
+  await mongoose.connect('mongodb+srv://ramitgzp:A6tZUiMX5lIjxstb@cluster0.llt2qkg.mongodb.net/photech');
 };
 mongoDB()
   .then(() => console.log("MongoDB Connected...."))
